e3298ca9400c646ca4b530e9cc0a0db800de2798
humor-404 <bigyamkarmacharya@gmail.com>
2025-12-13 15:00:08 +0545

    Removed

--- lua/user/plugins/vimBeGood.lua
+++
@@ -1,8 +0,0 @@
--- File: ~/.config/nvim/lua/user/plugins/vimBeGood.lua
-return {
-  {
-    "ThePrimeagen/vim-be-good",
-    cmd = "VimBeGood", -- lazy-load only when the command is used
-  },
-}
-
--- lua/user/polish.lua
+++
@@ -1,6 +0,0 @@
--- polish.lua is for small customizations, commands, autocommands, etc.
--- This will load custom LuaSnip snippets
-require("luasnip.loaders.from_lua").load({
-  paths = "~/.config/nvim/lua/user/snippets"
-})
-
